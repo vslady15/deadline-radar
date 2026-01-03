9587188e9608db2b4e0837d41f05d154e3cd1b84
vslady15 <hannahsecretho@gmail.com>
2026-01-03 15:57:15 +0000

    app.js

--- app.js
+++ app.js
@@ -403,4 +403,29 @@ function buildDescription(it, isReminder) {
 }
 
 function toICSDate(d) {
-  const pad = (n) => String(n
+  const pad = (n) => String(n).padStart(2, "0");
+  return `${d.getFullYear()}${pad(d.getMonth() + 1)}${pad(d.getDate())}`;
+}
+
+function toICSDateTime(d) {
+  const pad = (n) => String(n).padStart(2, "0");
+  const yyyy = d.getUTCFullYear();
+  const mm = pad(d.getUTCMonth() + 1);
+  const dd = pad(d.getUTCDate());
+  const hh = pad(d.getUTCHours());
+  const mi = pad(d.getUTCMinutes());
+  const ss = pad(d.getUTCSeconds());
+  return `${yyyy}${mm}${dd}T${hh}${mi}${ss}Z`;
+}
+
+function sanitizeICS(s) {
+  return String(s)
+    .replaceAll("\\", "\\\\")
+    .replaceAll("\n", "\\n")
+    .replaceAll("\r", "")
+    .replaceAll(",", "\\,")
+    .replaceAll(";", "\\;");
+}
+
+// Initial render
+render();
